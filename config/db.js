f5b86cb5c59cc626bf7cd326a9c6aed7017b3bb9
hitisha-goyani <goyanihitisha@gmail.com>
2025-11-13 21:04:04 +0530

    add

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect("mongodb://localhost:27017/bookstore");
 
     return connect; 
     
